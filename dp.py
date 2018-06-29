bf7cb0d486f24ff3ed5e1fb0fce7ffa86a28a0f4
MEITEL <meitel1014@gmail.com>
2018-06-29 14:32:07 +0900

    cached rdd

--- dp.py
+++ dp.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from pyspark import SparkConf, SparkContext
 import random
+import time
 
 conf = SparkConf().setMaster("local").setAppName("DynamicPlanning")
 sc = SparkContext(conf = conf)
@@ -12,14 +13,15 @@ N = 10
 A = 0
 
 random.seed(1)
-a = [random.randint(0, 10) for i in range(N)]  # 小切手の配列
+a = random.sample(range(15),N)  # 小切手の配列
 for i in range(N):
 #    print(a[i])
     if random.randint(0, 1) != 0:
         A += a[i]
-
+        
+start = time.time()
 # 初期化
-dp = [i for i in range(100)]
+dp = [i for i in range(A+1)]
 
 dp_rdd=sc.parallelize(dp)
 dp_rdd=dp_rdd.map(lambda x:(x,-1))
@@ -30,26 +32,39 @@ dp_rdd=dp_rdd.map(lambda x:(x[0],0)if x[0]==0 else x)
 dp_rdd.sortByKey()
 results=dp_rdd.collect()
 
-for result in results:
-    print("dp["+str(result[0])+"]:"+str(result[1]))
-
 # DP
 for i in range(N):
     for j in reversed(range(A+1)):
-        if(dp_rdd.lookup(j) == -1):
+        print(dp_rdd.lookup(j)[0])
+        if(dp_rdd.lookup(j)[0] == -1):
             continue
-        if ((a[i]+j <= A) and (dp_rdd.lookup(a[i]+j) == -1)):
+        if ((a[i]+j <= A) and (dp_rdd.lookup(a[i]+j)[0] == -1)):
+            dp_rdd=dp_rdd.map(lambda x:(x[0],a[i])if x[0]==a[i]+j else x)
+            dp_rdd.cache()
             #dp[a[i]+j] = a[i]
-            
-    if(dp_rdd.lookup(A) != -1):
+           
+    dp_rdd.uncache() 
+    dp_rdd.cache()
+    if(dp_rdd.lookup(A)[0] != -1):
         break
+
+elapsed_time = time.time() - start
+print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
+
 print("Answer:" + str(A))
 check = 0
 x = A
+
+dpresult=dp_rdd.collect()
+
+for dp in dpresult:
+    print("dp_rdd["+str(dp[0])+"]:"+str(dp[1]))
+
 while(1):
-    check += dp[x]
-    print(dp[x])
-    x = x-dp[x]
+    dpx = dp_rdd.lookup(x)[0]
+    check += dpx
+    print(dpx)
+    x = x-dpx
     if(x <= 0):
         break
 print("check="+str(check))
